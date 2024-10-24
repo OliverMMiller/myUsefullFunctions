d31aa72766beed4a9759b9c4910bf43c92d07c05
OliverMMiller <OliverMMiller@users.noreply.github.com>
2024-10-24 17:46:18 -0600

    fixed files and upgraded TextPrint

--- PygameTextTools.py
+++ PygameTextTools.py
@@ -7,6 +7,7 @@ class TextPrint:
         self.defaultLineLeight = line_height
         self.fontSize = fontSize
         self.font = pygame.font.Font(None, self.fontSize)
+        self.indentSize = 10
         self.textColor = (0, 0, 0)
         self.backgroundColor = None #(255, 255, 255)
 
@@ -19,3 +20,30 @@ class TextPrint:
         self.x = self.defaultX
         self.y = self.defaultY
         self.line_height = self.defaultLineLeight
+
+    def indent(self):
+        self.x += self.indentSize
+
+    def unindent(self):
+        self.x -= self.indentSize
+
+class SimpleTextPrint:
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 25)
+
+    def tprint(self, screen, text):
+        text_bitmap = self.font.render(text, True, (0, 0, 0))
+        screen.blit(text_bitmap, (self.x, self.y))
+        self.y += self.line_height
+
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+
+    def indent(self):
+        self.x += 10
+
+    def unindent(self):
+        self.x -= 10
